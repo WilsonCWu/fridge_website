ecc413444de39f60b80e31e350ad66e896eb3266
Wilson Wu <wilsoncwu@gmail.com>
2018-12-31 15:56:04 -0800

    init

---
+++ html/index.php
@@ -0,0 +1,23 @@
+<html>
+<p>htmltest</p>
+</html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "fridge";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO commands (date, time, command) values(CURRENT_DATE(), NOW(), 'phpCommand')";
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close(i);
+echo phpinfo();
+?>
